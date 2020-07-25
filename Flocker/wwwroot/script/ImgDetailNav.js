7242b6107b7fb17b0d43494b9264d06cd71d078a
abdul mudhir <abdulmudhirss@gmail.com>
2020-07-26 00:45:59 +0100

    udpated more visual itnerface

---
+++ Flocker/wwwroot/script/ImgDetailNav.js
@@ -0,0 +1,15 @@
+﻿
+
+const navButtons = document.querySelectorAll(".carousel__indicator");
+const imgContainer = document.querySelector(".carousel__images");
+
+for (let i = 0; i < navButtons.length; i++) {
+
+
+    navButtons[i].addEventListener("click", function (event) {
+
+        const imgUrl = event.target.attributes["src"].value;
+        imgContainer.setAttribute("src", imgUrl);
+
+    })
+}
